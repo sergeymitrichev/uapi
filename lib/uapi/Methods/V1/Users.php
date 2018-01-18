bdd201705e382793f953ebea8a83fbb507f1044a
Sergey <sergeymitrichev@yandex.ru>
2018-01-18 13:52:57 +0300

    Add method V1\Users\usersCreate

--- lib/uapi/Methods/V1/Users.php
+++ lib/uapi/Methods/V1/Users.php
@@ -37,4 +37,30 @@ trait Users
             $parameters
         );
     }
+    /**
+     * Create a user
+     *
+     * @param array  $user user data
+     *
+     * @throws \InvalidArgumentException
+     * @throws \APIuCoz\Exception\CurlException
+     * @throws \APIuCoz\Exception\InvalidJsonException
+     *
+     * @return \APIuCoz\Response\ApiResponse
+     */
+    public function usersCreate(array $user)
+    {
+        if (! count($user)) {
+            throw new \InvalidArgumentException(
+                'Parameter `customer` must contains a data'
+            );
+        }
+
+        return $this->client->makeRequest(
+            '/users',
+            "POST",
+            json_encode($user)
+        );
+    }
+
 }
